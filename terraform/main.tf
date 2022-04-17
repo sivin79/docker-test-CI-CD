444d1ef432ea26e5c2cce889c3ca53e6411c1d80
sivin79 <sivin@yandex.ru>
2022-04-17 17:22:15 +0300

    dev

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,10 +19,9 @@ terraform {
   backend "s3" {
     bucket = "test-bucket-for-lessons"                     // Bucket where to SAVE Terraform State
     key    = "test-Docker-CI-CD/tfstate/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "eu-west-1"                                   // Region where bycket created
+    region = "eu-west-1"                                   // Region  where bycket created
   }
 }
-
 
 data "aws_availability_zones" "available" {
   state = "available"
@@ -31,7 +30,6 @@ data "aws_availability_zones" "available" {
 output "aws_availability_zones_availabale" {
   value = data.aws_availability_zones.available.names[*]
 }
-
 
 data "terraform_remote_state" "network" {
   backend = "s3"
